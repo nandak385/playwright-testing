41ee424312029fe8aca0716da402dd0d03f87658
nandak385 <47389519+nandak385@users.noreply.github.com>
2026-03-03 21:38:54 +0530

    Initial commit

---
+++ tests/day1/primitiveDataType.js
@@ -0,0 +1,11 @@
+var firstName = "Nanda"
+var companyName = "HCL Tech"
+var mobileNumber = 9123456789 
+var isAutomation = true
+var hasPlaywright 
+
+console.log("First Name: ",firstName);
+console.log("Company Name: ",companyName);
+console.log("Phone number: ",mobileNumber);
+console.log("isAutomation : ",isAutomation);
+console.log(typeof hasPlaywright);
